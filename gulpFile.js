8058a01b3b1c67096b43094efc134bf69e7d34c0
Alejandro Ramos <lotusotho11@gmail.com>
2024-11-08 10:07:15 +0100

    Delete gulp

--- gulpFile.js
+++
@@ -1,6 +0,0 @@
-let gulp = require("gulp");
-let gzip = require("gulp-gzip");
-
-gulp.task("compress", function () {
-  return gulp.src(["./dist/**/*.*"]).pipe(gzip()).pipe(gulp.dest("./dist"));
-});
