f7aae43df7fd0ce1e9d2914c8c7c4e27fc42e2ca
Alejandro Ramos <lotusotho11@gmail.com>
2024-11-21 20:04:36 +0100

    Revert "Update app component"
    This reverts commit 7805fbb444cc171ab8a50c39c0a211ca92d65145.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, ngOnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 import { isPlatformBrowser } from '@angular/common';
 import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements ngOnInit {
   constructor(@Inject(PLATFORM_ID) private platformId: object) {}
 
   ngOnInit(): void {
