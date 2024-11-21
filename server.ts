1859b10c89a928b163ab3e84baa7828b8435ba77
Alejandro Ramos <lotusotho11@gmail.com>
2024-11-21 19:09:01 +0100

    remove gulp test

--- server.ts
+++ server.ts
@@ -1,12 +1,11 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
-import * as path from 'path';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
-import compression from 'compression';
 
+// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
@@ -18,42 +17,18 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
-  // Añadir middleware de compresión
-  server.use(compression());
-
-  // Servir archivos estáticos desde el directorio 'compressed'
-  server.use(
-    '/static',
-    express.static(path.join(__dirname, 'compressed'), {
-      setHeaders: (res, path) => {
-        if (path.endsWith('.gz')) {
-          res.setHeader('Content-Encoding', 'gzip');
-          if (path.endsWith('.js.gz')) {
-            res.setHeader('Content-Type', 'application/javascript');
-          } else if (path.endsWith('.css.gz')) {
-            res.setHeader('Content-Type', 'text/css');
-          }
-        }
-      },
+  // Example Express Rest API endpoints
+  // server.get('/api/**', (req, res) => { });
+  // Serve static files from /browser
+  server.get(
+    '**',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+      index: 'index.html',
     })
   );
 
-  // Middleware para servir archivos estáticos comprimidos
-  server.get('*.js', (req, res, next) => {
-    req.url = req.url + '.gz';
-    res.set('Content-Encoding', 'gzip');
-    res.set('Content-Type', 'application/javascript');
-    next();
-  });
-
-  server.get('*.css', (req, res, next) => {
-    req.url = req.url + '.gz';
-    res.set('Content-Encoding', 'gzip');
-    res.set('Content-Type', 'text/css');
-    next();
-  });
-
-  // Rutas que usan el motor de Angular
+  // All regular routes use the Angular engine
   server.get('**', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
@@ -75,6 +50,7 @@ export function app(): express.Express {
 function run(): void {
   const port = process.env['PORT'] || 4000;
 
+  // Start up the Node server
   const server = app();
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
